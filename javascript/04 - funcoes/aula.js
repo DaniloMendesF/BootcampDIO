2234aabd079e66eeea411bf332d88686812bfb12
DaniloMendesF <danilmendes@gmail.com>
2022-11-17 01:25:30 -0300

    Aula e exercício sobre funcoes

---
+++ javascript/04 - funcoes/aula.js
@@ -0,0 +1,20 @@
+// Aula sobre funções 
+
+function teste(){
+    console.log('teste')
+}
+
+function sayMyName(name){
+    console.log('Your name is: ' + name );
+}
+
+function quadrado(valor){
+    return valor * valor;
+}
+
+teste();
+
+console.log(quadrado(10) , quadrado(2));
+
+sayMyName('Danilo')
+
