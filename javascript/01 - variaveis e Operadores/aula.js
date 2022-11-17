59bf746e2680c615e06ca00e8edede1eff79acd7
DaniloMendesF <danilmendes@gmail.com>
2022-11-16 23:52:19 -0300

    Lista de exercícios javascript

---
+++ javascript/01 - variaveis e Operadores/aula.js
@@ -0,0 +1,46 @@
+
+
+let variavel = 10;
+const pi = 3.14; // Valor constante que não pode alterar o valor 
+
+// Operações Aritméticas 
+
+/*
+
+variavel = 20 +30;
+
+console.log(variavel)
+
+variavel = 20 - 30;
+
+console.log(variavel)
+
+variavel = 20 * 30;
+
+console.log(variavel)
+
+variavel = 20/30;
+
+console.log(variavel)
+
+variavel = 20 % 8;
+
+console.log(variavel)
+
+*/
+
+
+// Desafio Consumo
+
+const kmPorLitros = 12;
+const precoCombustivel = 5.79;
+const distanciaEmKm = 1580;
+let preco;
+
+preco = distanciaEmKm * precoCombustivel / kmPorLitros;
+
+console.log(preco.toFixed(2));
+
+
+
+
